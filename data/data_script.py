032f9683779e9430659157ac7401462fbc6b1dcd
Wiktoria Seweryn <wik.ant.s@gmail.com>
2024-12-08 13:42:19 +0100

    added Luxembourg

--- data/data_script.py
+++ data/data_script.py
@@ -2,7 +2,7 @@ import csv
 import pandas as pd
 std_labels = ['country','date','total_cases','new_cases','total_deaths','new_deaths','hosp_patients','icu_patients','total_vaccinations','people_vaccinated','new_vaccinations']
 additional_labels= ['population', 'population_density', 'median_age', 'hospital_beds_per_thousand', 'human_development_index' , 'weekly_hosp_admissions', 'weekly_icu_admissions', 'stringency_index']
-countries = ['Austria','Luxemburg', 'Switzerland','Germany', 'Poland', 'Liechtenstein']
+countries = ['Austria','Luxembourg', 'Switzerland','Germany', 'Poland', 'Liechtenstein']
 
 # for each line if country==choosen country, select choosen labels and write the columns for them into a new file
 #read csv as a pandas dataframe
