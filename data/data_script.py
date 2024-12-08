de10943c1ce85a3ae75d0da30733086ec67eca70
Wiktoria Seweryn <wik.ant.s@gmail.com>
2024-12-08 13:38:26 +0100

    adding basic data files

--- data/data_script.py
+++ data/data_script.py
@@ -1,12 +1,20 @@
 import csv
 import pandas as pd
-std_labels = ["country","date","total_cases","new_cases","total_deaths","new_deaths","hosp_patients","icu_patients","total_vaccinations","people_vaccinated","new_vaccinations"]
-add_labels= ["population", "population_density", "median_age", "hospital_beds_per_thousand", "human_development_index" , "weekly_hosp_admissions", "weekly_icu_admissions", "stringency_index"]
-countries = ["Austria", "Luxemburg", "Switzerland","Germany", "Poland", "Lichtenstein"]
+std_labels = ['country','date','total_cases','new_cases','total_deaths','new_deaths','hosp_patients','icu_patients','total_vaccinations','people_vaccinated','new_vaccinations']
+additional_labels= ['population', 'population_density', 'median_age', 'hospital_beds_per_thousand', 'human_development_index' , 'weekly_hosp_admissions', 'weekly_icu_admissions', 'stringency_index']
+countries = ['Austria','Luxemburg', 'Switzerland','Germany', 'Poland', 'Liechtenstein']
 
 # for each line if country==choosen country, select choosen labels and write the columns for them into a new file
+#read csv as a pandas dataframe
+df = pd.read_csv('raw_data/all_data.csv')
+#copy to the new df only important columns
+df = df[std_labels].copy()
+#creating new file for each country
+for countryname in countries:
 
+    new_df = df.loc[df['country'] == countryname]
+    if len(new_df.index) !=0:
+        new_df.to_csv(countryname+'.csv', index=False)
+    else:
+        print("No data for ", countryname)
 
-#df = pd.read_csv('all_data.csv')
-
-#df.to_csv('your_file_name.csv', index=False)
